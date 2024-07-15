5faac42d337aab5078f9be36cab3c2cfb30bfe3d
Santosh Shah <santoshofficial679@gmail.com>
2024-07-15 17:59:26 +0545

    learnt about hooks, state and more

--- 04-fragments/src/App.jsx
+++ 04-fragments/src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FoodItems from "./components/FoodItems";
 import ErrorMsg from "./components/ErrorMsg";
+import Container from "./components/Container";
+import FoodInput from "./components/FoodInput";
+import { useState } from "react";
 // import "./App.css";
 // function App() {
 // let foodItems = ["Dal", "Bhat", "Roti", "Apple", "Mango"];
@@ -60,15 +63,55 @@ import ErrorMsg from "./components/ErrorMsg";
 
 // implemention of props
 function App() {
-  let foodItems = ["Potato", "Tomato", "Bhat", "Roti", "Apple", "Mango"];
+  // let foodItems = ["Potato", "Tomato", "Bhat", "Roti", "Apple", "Mango"];
 
   // let foodItems = [];
 
+  // let textStateArr = useState("Vedas");
+  // let textToChange = textStateArr[0];
+  // let setTextState = textStateArr[1];
+
+  // let [textToChange, setTextState] = useState("Vedas");
+
+  let [foodItems, setFoodItems] = useState([
+    "Potato",
+    "Tomato",
+    "Bhat",
+    "Roti",
+    "Apple",
+  ]);
+
+  // let [foodItems, setFoodItems] = useState([]);
+
+  const onKeyDown = (event) => {
+    if (event.key === "Enter") {
+      let newFoodItem = event.target.value;
+      event.target.value = "";
+      let newItems = [...foodItems, newFoodItem];
+      setFoodItems(newItems);
+    }
+    // console.log(event);
+    // setTextState(event.target.value);
+  };
+
+  // const handleOnChange = (event) => {
+  //   console.log(event.target.value);
+  //   setTextState(`Current Value: ${event.target.value}`);
+  // };
+
   return (
     <>
-      <h1 className="food-heading">Healthy Food</h1>
-      <ErrorMsg items={foodItems} />
-      <FoodItems items={foodItems} />
+      <Container>
+        <h1 className="food-heading">Healthy Food</h1>
+        <FoodInput handleKeyDown={onKeyDown} />
+        {/* <p>{textToChange}</p> */}
+        <ErrorMsg items={foodItems} />
+        <FoodItems items={foodItems} />
+      </Container>
+
+      {/* <Container>
+        <p>My name is Santosh Shah</p>
+      </Container> */}
     </>
   );
 }
--- 04-fragments/src/components/Item.jsx
+++ 04-fragments/src/components/Item.jsx
@@ -5,9 +5,21 @@ import styles from "./Item.module.css";
 
 // destructuring all items
 const Item = ({ foodItems }) => {
+  const handleBuyButtonClicked = (event) => {
+    console.log(event);
+    console.log(`${foodItems} being bought`);
+  };
+
   return (
     <li className={`${styles["ss-item"]} list-group-item}`}>
       <span className={styles["ss-span"]}>{foodItems}</span>
+
+      <button
+        className={`${styles.button} btn btn-info`}
+        onClick={(event) => handleBuyButtonClicked(event)}
+      >
+        Buy
+      </button>
     </li>
   );
 };
